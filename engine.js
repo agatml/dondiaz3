42ef646c19c74abef93fb9733e93a3ea3bf37c63
Agatha Soares <agathavitoria.benetti@gmail.com>
2026-01-27 18:43:14 -0300

    add proj

---
+++ engine.js
@@ -0,0 +1,19 @@
+const toggleButton = document.getElementById('theme-toggle');
+const rootElement = document.documentElement;
+
+toggleButton.addEventListener('click', () => {
+    const isDark = rootElement.getAttribute('data-theme') === 'dark';
+
+    if (isDark) {
+        rootElement.removeAttribute('data-theme');
+        localStorage.removeItem('theme');
+    } else {
+        rootElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+});
+
+// Persistência
+if (localStorage.getItem('theme') === 'dark') {
+    rootElement.setAttribute('data-theme', 'dark');
+}
---
+++ hero-carousel.js
@@ -0,0 +1,12 @@
+const slides = document.querySelectorAll('.carousel-slide');
+let currentIndex = 0;
+
+function showNextSlide() {
+  slides[currentIndex].classList.remove('active');
+
+  currentIndex = (currentIndex + 1) % slides.length;
+
+  slides[currentIndex].classList.add('active');
+}
+
+setInterval(showNextSlide, 2500); // troca a cada 5s
