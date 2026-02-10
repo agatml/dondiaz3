600a3e5da1df800dd627cf4ecb148f696651a4d6
Agatha Soares <agathavitoria.benetti@gmail.com>
2026-02-10 13:12:17 -0300

    ok

--- hero-carousel.js
+++ hero-carousel.js
@@ -1,14 +1,13 @@
 const slides = document.querySelectorAll('.carousel-slide');
 let currentIndex = 0;
 
-function showNextSlide() {
+// ativa o primeiro slide imediatamente
+slides[currentIndex].classList.add('active');
+
+setInterval(() => {
   slides[currentIndex].classList.remove('active');
 
   currentIndex = (currentIndex + 1) % slides.length;
 
   slides[currentIndex].classList.add('active');
-}
-
-showNextSlide();
-
-setInterval(showNextSlide, 2500); 
+}, 2500);
