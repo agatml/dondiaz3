20cd64832609b1c627842b3a1fb4ef157a068553
Agatha Soares <agathavitoria.benetti@gmail.com>
2026-02-19 18:16:29 -0300

    menu alteração

--- menu.js
+++ menu.js
@@ -1,10 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.querySelector(".menu-toggle");
+const toggle = document.querySelector(".menu-toggle");
   const nav = document.querySelector(".nav");
+  const navLinks = document.querySelectorAll(".nav a");
 
   if (toggle && nav) {
+
     toggle.addEventListener("click", function () {
       nav.classList.toggle("active");
     });
-  }
-});
+
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        nav.classList.remove("active");
+      });
+    })}
